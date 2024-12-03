b4309aa1b649e153e942345900d8b2fe9057c2f9
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2024-12-03 19:13:12 -0300

    feat: ajustando o apontamento entre as páginas

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# aponta para a página principal
 def index(request):
     return render(request, 'galeria/index.html')
+
+# aponta para a página de imagem
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
